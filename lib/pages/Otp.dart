053d71169d7ac07b247408958c6a65db3c44c9df
akashL123 <akashbabu2511@gmail.com>
2023-03-20 10:31:25 +0530

    input loading

--- lib/pages/Otp.dart
+++ lib/pages/Otp.dart
@@ -2,6 +2,9 @@ import 'package:share/pages/Home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:another_flushbar/flushbar.dart';
+import 'package:another_flushbar/flushbar_helper.dart';
+import 'package:another_flushbar/flushbar_route.dart';
 
 class Otp extends StatefulWidget {
   final String verID;
@@ -17,17 +20,54 @@ class _OtpState extends State<Otp> {
   FirebaseAuth auth = FirebaseAuth.instance;
   String? user;
   Future<void> verifyOTP() async {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Center(child: CircularProgressIndicator());
+        });
     PhoneAuthCredential credential = PhoneAuthProvider.credential(
         verificationId: widget.verID, smsCode: otpController.text);
     await FirebaseAuth.instance.signInWithCredential(credential).then((value) {
       user = value.user?.uid;
+      Navigator.of(context).pop();
     });
+  }
+
+  // Future<void> verifyOTP() async {
+  //   String otp = otpController.text.trim();
+  //   if (otp.isEmpty || otp.length != 6) {
+  //     showToast("Please Enter a Valid The OTP");
+  //     return;
+  //   }
+
+  //   PhoneAuthCredential credential = PhoneAuthProvider.credential(
+  //     verificationId: widget.verID,
+  //     smsCode: otp,
+  //   );
+  //   try {
+  //     UserCredential authResult =
+  //         await FirebaseAuth.instance.signInWithCredential(credential);
+  //     user = authResult.user?.uid;
+  //     print(user);
+  //   } catch (e) {
+  //     showToast("Invalid OTP");
+  //     return;
+  //   }
+  // }
+
+  void showToast(String message) {
+    Flushbar(
+      message: message,
+      duration: Duration(seconds: 3),
+      messageColor: Colors.white,
+    ).show(context);
   }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double widgetWidth2 = screenWidth * 0.3;
+    double widgetWidth = screenWidth * 0.8;
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: Column(
@@ -50,33 +90,42 @@ class _OtpState extends State<Otp> {
             ),
           ),
           const SizedBox(),
-          TextField(
-            controller: otpController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Otp',
-              hintStyle: TextStyle(color: Colors.black),
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          Center(
+            child: SizedBox(
+              width: widgetWidth,
+              child: TextField(
+                controller: otpController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black, // set border color here
+                    ),
+                  ),
+                  hintText: 'Enter Your OTP',
+                  labelText: 'Otp',
+                  labelStyle: TextStyle(
+                      fontWeight: FontWeight.w500, color: Colors.black),
+                  hintStyle: TextStyle(color: Colors.black, fontSize: 13),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                ),
+              ),
             ),
           ),
           Center(
               child: SizedBox(
-                  width: widgetWidth2,
+                  width: widgetWidth,
                   child: ElevatedButton(
                     onPressed: () async {
                       try {
                         await verifyOTP();
-                        Fluttertoast.showToast(
-                          msg: "You are logged in successfully",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.red,
-                          textColor: Colors.white,
-                          fontSize: 16.0,
-                        );
+                        Flushbar(
+                          title: "You Logged In Successfully",
+                          message: "You Logged In Successfully",
+                          duration: Duration(seconds: 3),
+                        ).show(context);
                         Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
@@ -84,15 +133,11 @@ class _OtpState extends State<Otp> {
                           ),
                         );
                       } catch (e) {
-                        Fluttertoast.showToast(
-                          msg: "your login is failed",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.red,
-                          textColor: Colors.white,
-                          fontSize: 16.0,
-                        );
+                        Flushbar(
+                          title: "your login is failed",
+                          message: "your login is failed",
+                          duration: Duration(seconds: 3),
+                        ).show(context);
                       }
                     },
                     style: ElevatedButton.styleFrom(
